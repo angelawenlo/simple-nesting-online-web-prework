15cd83dcc5ae87bfb7ed5c813c133eb98fcafeb1
Angela Lo <angelawenlo@gmail.com>
2019-05-17 16:00:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,34 +2,3 @@
 def hopper
  programmer_hash = {}
 end
-
-def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-
-
-end
-
-def dennis_ritchies_language
-
-
-def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
-
-
-end
-
-def changing_alan
-
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-
-
-end
-
-def adding_to_dennis
-
-end
