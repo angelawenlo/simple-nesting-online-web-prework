54bea576537569b37fc8f8ca02368f7c5e2d4c71
Angela Lo <angelawenlo@gmail.com>
2019-05-17 16:32:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,6 +65,7 @@ def adding_matz
   }
   programmer_hash [:yukihiro_matsumoto] = {}
   programmer_hash [:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash [:yukihiro_matsumoto][:languages] = "Ruby"
   programmer_hash
   binding.pry
 end
